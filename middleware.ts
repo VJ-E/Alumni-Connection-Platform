3a3473a199955d9d5df7a22a85b1f4d229cba92a
VJ-E <kit27.am57@gmail.com>
2025-06-24 09:53:53 +0530

    middleware 504 correction..

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,29 @@ import { authMiddleware } from "@clerk/nextjs/server";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 export default authMiddleware({
-  publicRoutes: ["/sign-in"],
+  publicRoutes: [
+    "/sign-in",
+    "/sign-up",
+    "/api/webhook",
+    "/",
+    "/_next/static/*",
+    "/favicon.ico",
+    "/kit_logo.png",
+    "/banner.jpg",
+    "/login-banner.jpg",
+    "/default-avator.png"
+  ],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+  ],
 };
