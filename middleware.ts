f7044c10701960e8758c21d4ec83e196bf440c38
VJ-E <kit27.am57@gmail.com>
2025-06-24 15:58:24 +0530

    ...

--- middleware.ts
+++ middleware.ts
@@ -11,24 +11,13 @@ export default authMiddleware({
     "/api/uploadthing",
   ],
   ignoredRoutes: [
-    "/((?!api|trpc))(_next.*|.+.[w]+$)",
     "/api/webhook(.*)",
     "/_next/static/(.*)",
     "/favicon.ico",
-    "/public/(.*)"
   ]
 });
 
 // Stop Middleware running on static files and public folder
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
