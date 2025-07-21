633bf0264fe785bd65b88565dc7f26dba512559f
VJ-E <kit27.am57@gmail.com>
2025-07-21 17:37:00 +0530

    .,.,.,.,., (part 2/2)

--- middleware.ts
+++ middleware.ts
@@ -4,28 +4,31 @@ import { authMiddleware } from "@clerk/nextjs/server";
 // Please edit this to allow other routes to be public as needed.
 export default authMiddleware({
   publicRoutes: [
-    "/sign-in",
-    "/sign-up",
-    "/api/webhook",
     "/",
-    "/_next/static/*",
-    "/favicon.ico",
-    "/kit_logo.png",
-    "/banner.jpg",
-    "/login-banner.jpg",
-    "/default-avator.png"
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/api/webhook(.*)",
+    "/api/uploadthing",
   ],
+  ignoredRoutes: [
+    "/((?!api|trpc))(_next.*|.+.[w]+$)",
+    "/api/webhook(.*)",
+    "/_next/static/(.*)",
+    "/favicon.ico",
+    "/public/(.*)"
+  ]
 });
 
+// Stop Middleware running on static files and public folder
 export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - public folder
      */
-    "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
