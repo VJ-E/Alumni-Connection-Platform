d71821a8304fd1084eb489ef38e033fd149cd192
VJ-E <kit27.am57@gmail.com>
2025-08-28 12:59:00 +0530

    added events feature... (part 6/8)

--- components/shared/ProfilePhoto.tsx
+++ components/shared/ProfilePhoto.tsx
@@ -2,11 +2,15 @@ import Image from 'next/image'
 import React from 'react'
 import { Avatar, AvatarImage } from '../ui/avatar'
 
-const ProfilePhoto = ({ src }: { src: string }) => {
+const ProfilePhoto = ({ src, alt = "Profile photo" }: { src: string; alt?: string }) => {
     return (
         <div>
             <Avatar className='cursor-pointer'>
-                <AvatarImage src={src} alt="banner" />
+                <AvatarImage 
+                    src={src} 
+                    alt={alt}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                />
             </Avatar>
         </div>
     )
