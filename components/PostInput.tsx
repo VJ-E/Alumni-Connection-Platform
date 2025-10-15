14ad435f846e706482ae57cab51c3ada09bdbedc
VJ-E <kit27.am57@gmail.com>
2025-10-15 12:57:00 +0530

    added dynamic theme for all components to support toggle light-dark theme (part 6/8)

--- components/PostInput.tsx
+++ components/PostInput.tsx
@@ -19,13 +19,13 @@ const PostInput = () => {
     setOpen(true);
   };
   return (
-    <div className="bg-white p-4 m-2 md:m-0 border border-gray-300 rounded-lg">
+    <div className="bg-card text-card-foreground p-4 m-2 md:m-0 border border-border rounded-lg">
       <div className="flex items-center gap-3">
         <ProfilePhoto src={profile?.profilePhoto || "./default-avator.png"} />
         <Input
           type="text"
           placeholder="Share something..."
-          className="rounded-full hover:bg-gray-100 h-12 cursor-pointer"
+          className="rounded-full hover:bg-accent/50 h-12 cursor-pointer transition-colors"
           onClick={inputHandler}
         />
         <PostDialog
